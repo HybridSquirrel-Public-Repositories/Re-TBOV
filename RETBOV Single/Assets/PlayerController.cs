06982646c36762fa1a481e53fa8c9dd857d0f236
unknown <robert.squirrel@milfence.com>
2021-02-23 22:20:39 +0100

    migrated project from gitlab

---
+++ RETBOV Single/Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        // turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+         
+
+        if (Input.GetKeyDown("escape"))
+        {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+       
+        
+    }
+
+
+}
